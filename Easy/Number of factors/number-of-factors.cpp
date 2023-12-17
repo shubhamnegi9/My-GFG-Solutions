6008906815e8a395489439a101bb915eb4fb0444
shubhamnegi9 <64123186+shubhamnegi9@users.noreply.github.com>
2023-12-17 20:48:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of factors/number-of-factors.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int countFactors(int N){
+        int count = 0;
+        for(int i = 1; i*i <= N; i++) {
+            if(N % i == 0) {
+                count++;
+                if(N/i != i) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.countFactors(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
