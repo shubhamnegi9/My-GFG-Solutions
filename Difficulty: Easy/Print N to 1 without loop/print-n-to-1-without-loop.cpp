04de17a10ab541ebdf33ef55e3b181f204cf97c2
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-04-24 08:09:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.cpp
+++ Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.cpp
@@ -4,16 +4,37 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    void printNos(int N) {
-        // code here
-        if(N == 0)
+    
+    void printNto1(int N) {
+        if(N < 1)
             return;
+        
         cout << N << " ";
-        printNos(N-1);
+        printNto1(N-1);
+    }
+    
+    // Using backtracking
+    void printNto1(int i, int N) {
+        if(i > N)
+            return;
+        
+        printNto1(i+1, N);
+        cout << i << " ";
+    }
+  
+    void printNos(int N) {
+        
+        // Approach 1
+        // printNto1(N);
+        
+        // Approach 2
+        printNto1(1, N);
     }
 };
+
 
 //{ Driver Code Starts.
 /* Driver program to test printNos */
