2cb73c91411943a7cbf85c65f3b863e9e2768a4a
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-04-26 18:05:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.cpp
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -31,9 +31,8 @@ class Solution {
         for(int i = 0; i < n-1; i++) {
             int mini = i;
             for(int j = i+1; j < n; j++) {
-                if(arr[j] < arr[mini]) {
+                if(arr[j] < arr[mini])
                     mini = j;
-                }
             }
             swap(arr[i], arr[mini]);
         }
