9f01c53dc11fc3c745895f40c042d772e666462f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-04-27 11:56:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    // Brute Force Approach
+    // T.C. = O(nlogn) + O(n)
+    // S.C. = O(n)
+    int removeDuplicates1(vector<int>& arr) {
+        set<int> st;
+        for(int& ele: arr) {
+            st.insert(ele);
+        }
+        
+        int i = 0;
+        for(auto ele: st) {
+            arr[i] = ele;
+            i++;
+        }
+        
+        return st.size();
+    }
+    
+    // Optimal Approach 
+    // T.C. = O(n)
+    // S.C. = O(1)
+    int removeDuplicates2(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0;
+        for(int j = 1; j < n; j++) {
+            if(arr[j] != arr[i]) {
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+        
+        return i+1;
+    }
+    
+    int removeDuplicates(vector<int> &arr) {
+        // Brute Force Approach
+        // return removeDuplicates1(arr);
+        
+        // Optimal Approach 
+        return removeDuplicates2(arr);
+         
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.removeDuplicates(arr);
+        for (int i = 0; i < ans; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
