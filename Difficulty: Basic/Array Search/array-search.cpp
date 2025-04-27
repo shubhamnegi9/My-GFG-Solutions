775f2c33b15fdb645e6a856a345af50cac666bb1
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-04-27 13:40:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    
+    // T.C. = O(n)
+    // S.C. = O(1)
+    int search(vector<int>& arr, int x) {
+        int n = arr.size(), index = -1;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == x) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int testcases;
+    cin >> testcases;
+    cin.ignore(); // To ignore the newline character after reading testcases
+    while (testcases--) {
+        vector<int> arr;
+        int x;
+        string input;
+
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        getline(cin, input); // Read the next line for x
+        stringstream ss2(input);
+        ss2 >> x;
+
+        Solution ob;
+        cout << ob.search(arr, x) << endl; // Linear search
+        cout << '~' << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
