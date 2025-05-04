cfe75b1b122845d4d47843b9d5388004814c273f
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-04 13:44:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
+++ Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
@@ -8,76 +8,43 @@ using namespace std;
 
 class Solution {
   public:
-    // Brute Force Approach
-    // T.C. = O(mlogm) + O(nlogn) + O(m+n)
-    // S.C. = O(m+n) extra space for set + O(m+n) for returning answer
-    vector<int> findUnion1(vector<int> &a, vector<int> &b) {
-        set<int> st;
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        vector<int> temp;
         
-        for(int& ele: a) {
-            st.insert(ele);
-        }
-        
-        for(int& ele: b) {
-            st.insert(ele);
-        }
-        
-        vector<int> unionArr;
-        for(auto ele: st) {
-            unionArr.push_back(ele);
-        }
-        
-        return unionArr;
-    }
-    
-    // Optimal Approach
-    // T.C. = O(m+n)
-    // S.C. = O(m+n) for returning answer
-    vector<int> findUnion2(vector<int> &a, vector<int> &b) {
-        vector<int> unionArr;
-        int i = 0, j = 0;
         int m = a.size(), n = b.size();
+        int i = 0, j = 0;
         
         while(i < m && j < n) {
             if(a[i] <= b[j]) {
-                if(unionArr.size() == 0 || a[i] != unionArr.back()) {
-                    unionArr.push_back(a[i]);
+                if(temp.empty() || a[i] != temp.back()) {
+                    temp.push_back(a[i]);
                 }
                 i++;
             } else {
-                if(unionArr.size() == 0 || b[j] != unionArr.back()) {
-                    unionArr.push_back(b[j]);
+                if(temp.empty() || b[j] != temp.back()) {
+                    temp.push_back(b[j]);
                 }
                 j++;
             }
         }
         
         while(i < m) {
-            if(unionArr.size() == 0 || a[i] != unionArr.back()) {
-                unionArr.push_back(a[i]);
+            if(temp.empty() || a[i] != temp.back()) {
+                temp.push_back(a[i]);
             }
             i++;
         }
         
         while(j < n) {
-            if(unionArr.size() == 0 || b[j] != unionArr.back()) {
-                unionArr.push_back(b[j]);
-            }
+          if(temp.empty() || b[j] != temp.back()) {
+                temp.push_back(b[j]);
+            }  
             j++;
         }
         
-        return unionArr;
-    }
- 
-    // a,b : the arrays
-    // Function to return a list containing the union of the two arrays.
-    vector<int> findUnion(vector<int> &a, vector<int> &b) {
-        // Brute Force Approach
-        // return findUnion1(a, b);
-        
-        // Optimal Approach
-        return findUnion2(a, b);
-
+        return temp;
     }
 };
 
