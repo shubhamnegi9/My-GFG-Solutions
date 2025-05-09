67c1c49f389871ae000172f0987d4723ddc764d5
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-09 23:45:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    // Brute Force Approach
+    // T.C. = O(n^2)
+    // S.C. = O(n) for storing result
+    vector<int> leaders1(vector<int>& arr, int n) {
+        vector<int> result;
+        for(int i = 0; i < n-1; i++) {
+            bool isLeader = true;
+            for(int j = i+1; j < n; j++) {
+                if(arr[j] > arr[i]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+            if(isLeader) {
+                result.push_back(arr[i]);
+            }
+        }
+        result.push_back(arr[n-1]);     // Last element is always a leader
+        return result;
+    }
+    
+    // Optimal Approach
+    // T.C. = O(n) + O(n) for reverse function
+    // S.C. = O(n) for storing result
+    vector<int> leaders2(vector<int>& arr, int n) {
+        vector<int> result;
+        int maxEle = INT_MIN;
+        
+        for(int i = n-1; i >= 0; i--) {
+            if(arr[i] >= maxEle) {
+                result.push_back(arr[i]);
+                maxEle = arr[i];
+            }
+        }
+        
+        reverse(result.begin(), result.end());  // For printing in same order in which they appear
+        return result;
+    }
+    
+    vector<int> leaders(vector<int>& arr) {
+        int n = arr.size();
+        // Brute Force Approach
+        // return leaders1(arr, n);
+        
+        // Optimal Approach
+        return leaders2(arr, n);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.leaders(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no leaders are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
