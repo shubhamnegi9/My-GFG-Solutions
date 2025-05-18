2966d81f831431ba29e9f2736e91d4a03ed69327
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-18 17:35:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.cpp
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -11,29 +11,20 @@ using namespace std;
 class Solution {
   public:
     // Function to sort the array using bubble sort algorithm.
-    
-    void recursiveBubbleSort(vector<int>& arr, int n) {
-        if(n <= 1) {
-            return;
-        }
-        
-        bool isSwapped = false;
-        for(int j = 0; j <= n-2; j++) {
-            if(arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
-                isSwapped = true;
-            }   
-        }
-        
-        if(isSwapped == false)
-            return;
-        
-        recursiveBubbleSort(arr, n-1);
-    }
-    
+    // T.C. = O(n^2) in worst case, O(n) in best case
     void bubbleSort(vector<int>& arr) {
         int n = arr.size();
-        recursiveBubbleSort(arr, n);
+        for(int i = n-1; i >= 0; i--) {
+            bool isSwapped = false;
+            for(int j = 0; j < i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    swap(arr[j], arr[j+1]);
+                    isSwapped = true;
+                }
+            }
+            if(isSwapped == false)
+                break;
+        }
     }
 };
 
