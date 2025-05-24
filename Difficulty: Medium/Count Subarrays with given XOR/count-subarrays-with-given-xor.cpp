2ee0e78ae4daacb3af03b96a8f429e50982fb6cb
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-24 17:44:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
@@ -1,38 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    
-    // Brute Force Approach
-    // T.C. = O(n^3)
-    // S.C. = O(1)
-    long subarrayXor1(vector<int> &arr, int k, int n) {
-        long count = 0;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                int XR = 0;
-                for(int l = i; l <= j; l++) {
-                    XR ^= arr[l];
-                }
-                if(XR == k) {
-                    count++;
-                }
-            } 
-        }
-        
-        return count;
-    }
-    
-    // Better Approach
+    // Brute force approach
     // T.C. = O(n^2)
     // S.C. = O(1)
-    long subarrayXor2(vector<int> &arr, int k, int n) {
+    long subarrayXor1(vector<int> &arr, int k, int n) {
         long count = 0;
         
         for(int i = 0; i < n; i++) {
@@ -40,10 +11,9 @@ class Solution {
             for(int j = i; j < n; j++) {
                 XR ^= arr[j];
                 
-                if(XR == k) {
+                if(XR == k)
                     count++;
-                }
-            } 
+            }
         }
         
         return count;
@@ -51,16 +21,15 @@ class Solution {
     
     // Optimal Approach
     // T.C. = O(n)
-    // S.C. = O(n) for map
-    long subarrayXor3(vector<int> &arr, int k, int n) {
+    // S.C. = O(n)
+    long subarrayXor2(vector<int> &arr, int k, int n) {
+        long count = 0, XR = 0;
         unordered_map<int, int> mpp;
         mpp[0] = 1;
-        int XR = 0;
-        long count = 0;
         
-        for(int& ele: arr) {
-            XR ^= ele;
-            count += mpp[XR^k];
+        for(int i = 0; i < n; i++) {
+            XR ^= arr[i];
+            count += mpp[XR ^ k];
             mpp[XR]++;
         }
         
@@ -69,46 +38,10 @@ class Solution {
     
     long subarrayXor(vector<int> &arr, int k) {
         int n = arr.size();
-        
-        // Brute Force Approach
+        // Brute force approach
         // return subarrayXor1(arr, k, n);
         
-        // Better Approach
-        // return subarrayXor2(arr, k, n);
-        
         // Optimal Approach
-        return subarrayXor3(arr, k, n);
+        return subarrayXor2(arr, k, n);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore(); // Ignore the newline after the number of test cases
-
-    while (tc-- > 0) {
-        string input;
-        getline(cin, input); // Read the whole line for the array
-
-        stringstream ss(input);
-        vector<int> arr;
-        int num;
-
-        while (ss >> num) {
-            arr.push_back(num); // Push numbers into the vector
-        }
-
-        int k;
-        cin >> k;
-        cin.ignore(); // Ignore the newline after k
-
-        Solution obj;
-        cout << obj.subarrayXor(arr, k) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
