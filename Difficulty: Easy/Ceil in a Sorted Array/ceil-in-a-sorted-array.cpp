df749b2032de6188e01b93d901a17d63831ad6ed
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-05-26 20:29:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.cpp
+++ Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.cpp
@@ -1,58 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 class Solution {
   public:
     int findCeil(vector<int>& arr, int x) {
-        // Finding ceil is equivalent to finding lower bound
-        
-        int n = arr.size();
-        int low = 0, high = n-1;
-        int result = -1;
+        int low = 0, high = arr.size()-1;
+        int index = -1;
         
         while(low <= high) {
             int mid = low+(high-low)/2;
             
-            if(arr[mid] < x) {
-                low = mid+1;
-            } else {
-                result = mid;
+            if(arr[mid] >= x) {
+                index = mid;
                 high = mid-1;
+            } else {
+                low = mid+1;
             }
         }
         
-        return result;
+        return index;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string xs;
-        getline(cin, xs);
-        int x = stoi(xs);
-        Solution ob;
-        int ans = ob.findCeil(arr, x);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
