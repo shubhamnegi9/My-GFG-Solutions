f4ed00e1bf9b3142754a008b6c24c21912903828
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-07-25 09:31:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bit Manipulation/bit-manipulation.cpp
+++ Difficulty: Easy/Bit Manipulation/bit-manipulation.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
-    void bitManipulation(int num, int i) {
-        // Get ith bit
+    
+    void bitManiputaion1(int num, int i) {
         string b = "";
         int temp = num;
         while(temp) {
@@ -15,13 +15,23 @@ class Solution {
         int len = b.length();
         char res = ((i <= len) ? b[len-i] : '0');                // i is 1 based indexing
         cout << res << " ";
+    }
+    
+    void bitManipulation(int num, int i) {
+        // Get ith bit
+        // Brute Force
+        // bitManiputaion1(num, i);
+        
+        // Optimal
+        int n1 = num;
+        cout << ((n1 & (1 << (i-1))) >> (i-1)) << " ";     // i is 1 based indexing
         
         // Set ith bit
-        int n1 = num;
-        cout << (n1 | (1 << (i-1))) << " ";     // i is 1 based indexing
+        int n2 = num;
+        cout << (n2 | (1 << (i-1))) << " ";     // i is 1 based indexing
         
         // Clear ith bit
-        int n2 = num;
-        cout << (n2 & ~(1 << (i-1)));           // i is 1 based indexing
+        int n3 = num;
+        cout << (n3 & ~(1 << (i-1)));           // i is 1 based indexing
     }
 };
