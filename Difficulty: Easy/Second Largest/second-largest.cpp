68738a6ca6b4ce5bec40dc8acaa67f5306a17312
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-10-14 23:55:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.cpp
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -1,101 +1,75 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     // Brute Force Approach
-    // T.C. = O(nlogn) + O(n)
+    // T.C. = O(nlogn) + O(n) = O(nlogn)
     // S.C. = O(1)
-    int getSecondLargest1(vector<int> &arr, int n) {
+    int getSecondLargest1(vector<int> &arr) {
+        int sLargest = -1;
         sort(arr.begin(), arr.end());
         int largest = arr.back();
         
-        for(int i = n-2; i >= 0; i--) {
-            if(arr[i] != largest)
-                return arr[i];
+        for(int i = arr.size()-1; i >= 0; i--) {
+            if(arr[i] != largest) {
+                sLargest = arr[i];
+                break;
+            }
         }
         
-        return -1;
+        return sLargest;
     }
     
     // Better Approach
-    // T.C. = O(n) + O(n)
+    // T.C. = O(2n)
     // S.C. = O(1)
-    int getSecondLargest2(vector<int> &arr, int n) {
-        int largest = -1;
+    int getSecondLargest2(vector<int> &arr) {
+        int largest = arr[0];
+        int n = arr.size();
         
-        for(int& ele: arr) {
-            largest = max(largest, ele);
+        for(int i = 1; i < n; i++) {
+            if(arr[i] > largest) 
+                largest = arr[i];
         }
         
-        int secLargest = -1;
-        for(int i = 0; i < n; i++) {
-            if(arr[i] > secLargest && arr[i] != largest)
-                secLargest = arr[i];
+        int sLargest = -1;
+        for(int i = 1; i < n; i++) {
+            if(arr[i] > sLargest && arr[i] != largest) {
+                sLargest = arr[i];
+            }
         }
         
-        return secLargest;
+        return sLargest;
     }
     
     // Optimal Approach
     // T.C. = O(n)
     // S.C. = O(1)
-    int getSecondLargest3(vector<int> &arr, int n) {
-        int largest = -1, secLargest = -1;
+    int getSecondLargest3(vector<int> &arr) {
+        int n = arr.size();
+        int largest = arr[0];
+        int sLargest = -1;
         
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i < n; i++) {
             if(arr[i] > largest) {
-                secLargest = largest;
+                sLargest = largest;
                 largest = arr[i];
             }
-            else if(arr[i] > secLargest && arr[i] != largest)
-                secLargest = arr[i];
+            else if(arr[i] != largest && arr[i] > sLargest) {
+                sLargest = arr[i];
+            }
         }
         
-        return secLargest;
+        return sLargest;
     }
     
+    
     int getSecondLargest(vector<int> &arr) {
-        int n = arr.size();
-        
         // Brute Force Approach
-        // return getSecondLargest1(arr, n);
+        // return getSecondLargest1(arr);
         
         // Better Approach
-        // return getSecondLargest2(arr, n);
+        // return getSecondLargest2(arr);
         
         // Optimal Approach
-        return getSecondLargest3(arr, n);
+        return getSecondLargest3(arr);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        int ans = ob.getSecondLargest(arr);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
