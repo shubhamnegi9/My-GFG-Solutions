2cc1258a6a1d3d9941d56f0cc548ce0b9acb666e
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-10-14 23:17:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.cpp
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -1,34 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int factorial(int n) {
-        // Using functional recursion
-        if(n == 0)
+        if(n <= 1)
             return 1;
+        
         return n * factorial(n-1);
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        Solution ob;
-        cout << ob.factorial(N) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
