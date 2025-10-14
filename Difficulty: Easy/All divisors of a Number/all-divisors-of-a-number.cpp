1047780d5b14d2175ab4d88bf27017a924252b2b
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-10-14 23:11:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
@@ -1,42 +1,37 @@
 class Solution {
   public:
-    // Brute Force Approach
-    // T.C. = O(n)
-    // S.C. = O(1)
-    void print_divisors1(int n) {
+    
+    void printDivisors1(int n) {
         for(int i = 1; i <= n; i++) {
-            if(n % i == 0)
+            if(n%i == 0)
                 cout << i << " ";
         }
         cout << endl;
     }
     
-    // Optimal Approach
-    // T.C. = O(sqrt(n)) + O(mlogm). m = no. of divisors
-    // S.C. = O(1) for solving problem, O(m) for returning answer
-    void print_divisors2(int n) {
-        vector<int> arr;
+    void printDivisors2(int n) {
+        vector<int> ans;
         for(int i = 1; i*i <= n; i++) {
-            if(n % i == 0) {
-                arr.push_back(i);
-                
-                if((n/i) != i)
-                    arr.push_back(n/i);
+            if(n%i == 0) {
+                ans.push_back(i);
+                if(i != (n/i)) {
+                    ans.push_back(n/i);
+                }
             }
         }
         
-        sort(arr.begin(), arr.end());
+        sort(ans.begin(), ans.end());
         
-        for(int& num: arr) {
+        for(int& num: ans) {
             cout << num << " ";
         }
     }
     
     void print_divisors(int n) {
-        // Brute Force Approach
-        // print_divisors1(n);
+        // Brute Force
+        // printDivisors1(n);
         
         // Optimal Approach
-        print_divisors2(n);
+        printDivisors2(n);
     }
 };
