30aceaa710c4c1a64f2df21c79579450edc599ed
Shubham Negi <64123186+shubhamnegi9@users.noreply.github.com>
2025-10-14 23:59:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.cpp
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.cpp
@@ -1,72 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
     // Brute Force Approach
-    // T.C. = O(nlogn)
+    // T.C. = O(n^2)
     // S.C. = O(1)
-    bool arraySorted1(vector<int>& arr, int n) {
-        vector<int> sortedArr = arr;
+    bool isSorted1(vector<int>& arr) {
+        int n = arr.size();
         
-        sort(sortedArr.begin(), sortedArr.end());
-        
-        return sortedArr == arr;
-    }
-    
-    // Optimal Approach
-    // T.C. = O(n)
-    // S.C. = O(1)
-    bool arraySorted2(vector<int>& arr, int n) {
-
-        for(int i = 0; i < n-1; i++) {
-            if(arr[i] > arr[i+1])
-                return false;
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                if(arr[j] < arr[i])
+                    return false;
+            }
         }
         
         return true;
     }
     
-    bool arraySortedOrNot(vector<int>& arr) {
-        int n = arr.size();
-        
+    bool isSorted(vector<int>& arr) {
         // Brute Force Approach
-        // return arraySorted1(arr, n);
-        
-        // Optimal Approach
-        return arraySorted2(arr, n);
+        return isSorted1(arr);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-
-    while (t--) {
-        string line;
-        getline(cin, line);
-        stringstream ss(line);
-        vector<int> nums;
-        int num;
-        while (ss >> num) {
-            nums.push_back(num);
-        }
-        Solution ob;
-        bool ans = ob.arraySortedOrNot(nums);
-        cout << (ans ? "true" : "false") << endl;
-        cout << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
